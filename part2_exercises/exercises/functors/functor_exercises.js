99c807ee8f53bc0ef8bbd5a7f9abbe6586e3ee8a
Salman  Salman <solomanasoloman@gmail.com>
2016-12-05 08:56:17 -0800

    finished functor problems

--- part2_exercises/exercises/functors/functor_exercises.js
+++ part2_exercises/exercises/functors/functor_exercises.js
@@ -6,7 +6,7 @@ var _ = require('ramda');
 // ==========
 // Use _.add(x,y) and _.map(f,x) to make a function that increments a value inside a functor
 
-var ex1 = undefined;
+var ex1 = _.map(_.add(1));
 
 
 
@@ -15,7 +15,7 @@ var ex1 = undefined;
 // Use _.head to get the first element of the list
 var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
 
-var ex2 = undefined;
+var ex2 = _.map(_.head);
 
 
 
@@ -26,7 +26,7 @@ var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
 
 var user = { id: 2, name: "Albert" };
 
-var ex3 = undefined;
+var ex3 = _.map(compose(_.head), safeProp('name'));
 
 
 
@@ -38,7 +38,7 @@ var ex4 = function (n) {
   if (n) { return parseInt(n); }
 };
 
-var ex4 = undefined;
+var ex4 = _.compose(_.map(parseInt), Maybe.of);
 
 
 
@@ -50,12 +50,13 @@ var ex4 = undefined;
 var getPost = function (i) {
   return new Task(function(rej, res) {
     setTimeout(function(){
-      res({id: i, title: 'Love them futures'})  
+      res({id: i, title: 'Love them futures'})
     }, 300)
   });
 };
 
-var ex5 = undefined;
+var upperTitle = _.compose(toUpperCase, _.prop('title'));
+var ex5 = _.compose(_.map(upperTitle), getPost);
 
 
 
@@ -69,7 +70,7 @@ var checkActive = function(user) {
  return user.active ? Right.of(user) : Left.of('Your account is not active')
 };
 
-var ex6 = undefined;
+var ex6 = _.compose(_.map(showWelcome), checkActive)
 
 
 
@@ -94,6 +95,6 @@ var save = function(x) {
   });
 };
 
-var ex8 = undefined;
+var ex8 = _.compose(either(IO.of, save), ex7)
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
